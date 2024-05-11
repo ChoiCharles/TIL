9e179ebbdc606d5a5fbd23a562b2bed05064d363
ChoiCharles <ckstjr3592@naver.com>
2024-05-11 21:38:43 +0900

    240511

--- java/workspace/thisisjava/src/ch05/sec03/ReferenceVariableCompareExample.java
+++ java/workspace/thisisjava/src/ch05/sec03/ReferenceVariableCompareExample.java
@@ -2,6 +2,18 @@ package ch05.sec03;
 
 public class ReferenceVariableCompareExample {
 	public static void main(String[] args) {
+		int[] arr1;
+		int[] arr2;
+		int[] arr3;
 		
+		arr1 = new int[] {1, 2, 3};
+		arr2 = new int[] {1, 2, 3};
+		arr3 = arr2;
+		
+		// 값은 같지만 다른 배열을 참조하므로 false
+		System.out.println(arr1 == arr2);
+		
+		// 같은 배열을 참조하므로 true
+		System.out.println(arr2 == arr3);
 	}
 }
